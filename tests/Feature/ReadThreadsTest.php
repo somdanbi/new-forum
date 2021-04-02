89da8035b4529f99ce8f2071d83fd867a687d922
Pc Elementary Os 2021 <bubu@outlook.com.pe>
2021-04-02 04:38:37 -0500

    Just Renaming ThreadsTest to ReadThreadsTest

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -5,7 +5,7 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ThreadsTest extends TestCase
+class ReadThreadsTest extends TestCase
 {
 
     /** @test */
