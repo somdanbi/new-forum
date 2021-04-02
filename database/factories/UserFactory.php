5787cc41c8a08141b063a8c1574b975842797170
Pc Elementary Os 2021 <bubu@outlook.com.pe>
2021-04-02 03:51:38 -0500

    Creating Factories

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,10 +17,47 @@ use Faker\Generator as Faker;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
+        'name'              => $faker->name,
+        'email'             => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
+        'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'remember_token'    => Str::random(10),
+    ];
+});
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories/ Thread
+|--------------------------------------------------------------------------
+|
+*/
+$factory->define(App\Thread::class, function ($faker) {
+    return [
+
+        'user_id' => function () {
+            return factory('App\User')->create()->id;
+        },
+
+        'title' => $faker->sentence,
+        'body'  => $faker->paragraph
+    ];
+});
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories/ Reply
+|--------------------------------------------------------------------------
+|
+*/
+$factory->define(App\Reply::class, function ($faker) {
+    return [
+        'user_id'   => function () {
+            return factory('App\User')->create()->id;
+        },
+        'thread_id' => function () {
+            return factory('App\Thread')->create()->id;
+        },
+
+        'body' => $faker->paragraph
     ];
 });
