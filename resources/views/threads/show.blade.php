ad4516e08f97479de293eda06f59222fec5efc99
Pc Elementary Os 2021 <bubu@outlook.com.pe>
2021-04-05 05:00:54 -0500

    5.1 1. Adding Reply form for Threads
    Now just..if you are log in can reply a thread.

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -27,5 +27,29 @@
             </div>
         </div>
 
+        @if(auth()->check())
+            <div class="row justify-content-center">
+                <div class="col-md-8 mt-4">
+                    <form method="post" action="{{ $thread->path(). '/replies' }}">
+                        @csrf
+                        <div class="form-group">
+                        <textarea name="body" id="body" rows="5" placeholder="Something to say?" class="form-control">
+
+                        </textarea>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Post</button>
+
+                    </form>
+
+                </div>
+            </div>
+        @else
+            <p class="text-center">Please
+                <a href="{{ route('login') }}">Sign in</a>
+                to participate in this discussion
+            </p>
+        @endif
+
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/threads', function () {
+Route::get('/', function () {
     return view('welcome');
 });
 
