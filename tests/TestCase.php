9696b3cb49fd595b6539ba4c360f860e39f11411
Pc Elementary Os 2021 <bubu@outlook.com.pe>
2021-04-06 03:53:44 -0500

    Adamwathan, Hat Tip withExceptionHandling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,8 @@
 
 namespace Tests;
 
+use App\Exceptions\Handler;
+use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
@@ -14,6 +16,35 @@ abstract class TestCase extends BaseTestCase
         $user = $user ?: create('App\User');
         $this->actingAs($user);
 
+        return $this;
+    }
+
+
+    // Hat tip, @adamwathan.
+    protected function disableExceptionHandling()
+    {
+        $this->oldExceptionHandler = $this->app->make(ExceptionHandler::class);
+        $this->app->instance(ExceptionHandler::class, new class extends Handler
+        {
+
+            public function __construct()
+            {
+            }
+
+            public function report(\Exception $e)
+            {
+            }
+
+            public function render($request, \Exception $e)
+            {
+                throw $e;
+            }
+        });
+    }
+
+    protected function withExceptionHandling()
+    {
+        $this->app->instance(ExceptionHandler::class, $this->oldExceptionHandler);
         return $this;
     }
 }
