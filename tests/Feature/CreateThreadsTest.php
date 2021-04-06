25e9f3a6f46123e71066cae742b87ee7b5d2cf7e
Pc Elementary Os 2021 <bubu@outlook.com.pe>
2021-04-06 03:03:41 -0500

    Creating signIn Method

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -16,7 +16,7 @@ class CreateThreadsTest extends TestCase
     function an_authenticated_user_can_create_new_forum_threads()
     {
         // Given we have a signed user
-        $this->actingAs(make('App\User'));
+        $this->signIn();
 
         // when we hit the endpoint to createa new thread
         $thread = make('App\Thread');
