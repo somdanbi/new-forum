630b201d02bb1f093fe675065804922c536b7338
Pc Elementary Os 2021 <bubu@outlook.com.pe>
2021-05-04 05:55:52 -0500

    Reformating Code.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,8 +24,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        \View::composer('*', function ($view){
-            $view->with('channels', \App\Channel::all());
-        });
+        \View::share('channels', \App\Channel::all());
     }
 }
