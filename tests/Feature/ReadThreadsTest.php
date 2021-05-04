9b38a3cac2d22085035676e1277d7025cbb46ad3
Pc Elementary Os 2021 <bubu@outlook.com.pe>
2021-05-04 04:48:19 -0500

    Channel Slug ok

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -44,4 +44,18 @@ class ReadThreadsTest extends TestCase
 
 
     }
+
+    /** @test */
+    function a_user_can_filter_threads_according_to_a_tag()
+    {
+        $channel = create('App\Channel');
+        $threadInChannel = create('App\Thread',['channel_id'=> $channel->id]);
+        $threadNotInChannel = create('App\Thread');
+
+        $this->get('/threads/'. $channel->slug)
+            ->assertSee($threadInChannel->title)
+            ->assertDontSee($threadNotInChannel->title);
+    }
+
+
 }
