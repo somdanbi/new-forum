1854f6f40d98f2c80a60dfff666bb557e68e0930
Pc Elementary Os 2021 <bubu@outlook.com.pe>
2021-05-05 06:20:32 -0500

    1. splitting divs
    col-8 and col-4

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container">
-        <div class="row justify-content-center">
+        <div class="row">
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">
@@ -14,42 +14,57 @@
                         {{ $thread->body }}
                     </div>
                 </div>
-            </div>
-        </div>
 
-        <div class="row justify-content-center">
-            <div class="col-md-8">
                 @foreach($thread->replies as $reply)
                     @include('threads.reply')
                 @endforeach
 
 
-            </div>
-        </div>
+                @if(auth()->check())
 
-        @if(auth()->check())
-            <div class="row justify-content-center">
-                <div class="col-md-8 mt-4">
                     <form method="post" action="{{ $thread->path(). '/replies' }}">
                         @csrf
                         <div class="form-group">
-                        <textarea name="body" id="body" rows="5" placeholder="Something to say?" class="form-control">
+                <textarea name="body" id="body" rows="5" placeholder="Something to say?" class="form-control">
 
-                        </textarea>
+                </textarea>
                         </div>
 
                         <button type="submit" class="btn btn-primary">Post</button>
 
                     </form>
+                @else
+                    <p class="text-center">Please
+                        <a href="{{ route('login') }}">Sign in</a>
+                        to participate in this discussion
+                    </p>
+                @endif
+
+            </div>
+
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-header">
+
+                    </div>
+
+                    <div class="card-body">
+                        <p>This thread was published
+                            {{$thread->created_at->diffForHumans()}}
+                            by: <a href="#">{{ $thread->creator->name }}</a>,
+                            and currently has
+                            {{ $thread->replies->count() }} comments.
+
+                        </p>
+
+                    </div>
 
                 </div>
+
             </div>
-        @else
-            <p class="text-center">Please
-                <a href="{{ route('login') }}">Sign in</a>
-                to participate in this discussion
-            </p>
-        @endif
+
+
+        </div>
 
     </div>
 @endsection
