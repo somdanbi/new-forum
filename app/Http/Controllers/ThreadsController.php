4301559b50fbc6ac9f001c4b8fb8e1255e9482a2
Pc Elementary Os 2021 <bubu@outlook.com.pe>
2021-05-05 06:26:01 -0500

    Displying array of show

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -41,6 +41,7 @@ class ThreadsController extends Controller
      */
     public function show($channelId, Thread $thread)
     {
+        return $thread;
         return view('threads.show', compact('thread'));
     }
 
