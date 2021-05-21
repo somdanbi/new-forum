aae12000b8277562f8631f9501253d79a5cd5b41
Pc Elementary Os 2021 <bubu@outlook.com.pe>
2021-05-21 06:01:28 -0500

    just replies array.
    just see the image.

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -44,7 +44,7 @@ class ThreadsController extends Controller
      */
     public function show($channelId, Thread $thread)
     {
-        return $thread->load('replies.favorites')->load('replies.owner');
+        return $thread->replies;
         return view('threads.show', [
             'thread'  => $thread,
             'replies' => $thread->replies()->paginate(5)
