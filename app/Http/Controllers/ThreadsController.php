0c3cede353fc317f53eb0c5b19b2a6d69bd200c5
Pc Elementary Os 2021 <bubu@outlook.com.pe>
2021-05-21 05:28:57 -0500

    array with replies
    now whe you hit the same point
    threads/magnam/2
    
    you can load all replies associated with it.
    
    it will display the same array you saw in the last commit, but there will be another array in replies section (in this case 6 replies)

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -44,7 +44,7 @@ class ThreadsController extends Controller
      */
     public function show($channelId, Thread $thread)
     {
-        return $thread;
+        return $thread->load('replies');
         return view('threads.show', [
             'thread'  => $thread,
             'replies' => $thread->replies()->paginate(5)
