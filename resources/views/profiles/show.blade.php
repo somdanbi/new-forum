fd83950c834cde674835d8ea1bdd16dd4190e779
Pc Elementary Os 2021 <bubu@outlook.com.pe>
2021-05-22 06:13:46 -0500

    profiles
    center some objects

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header bg-success">
-                        {{ $profileUser->name }}
+                    <div class="card-header">
+                        <h2>{{ $profileUser->name }}</h2>
                         <small>Since: {{ $profileUser->created_at->diffForHumans() }}</small>
                     </div>
 
