bf612ce11aff7cbbb016b8122d294f960ec59bf9
Pc Elementary Os 2021 <bubu@outlook.com.pe>
2021-05-22 04:27:06 -0500

    Refactoring quering in show
    page: /threads/magnam/2
    from 26 queries to 16

--- app/Thread.php
+++ app/Thread.php
@@ -31,7 +31,8 @@ class Thread extends Model
 
     public function replies()
     {
-        return $this->hasMany(Reply::class);
+        return $this->hasMany(Reply::class)
+            ->withCount('favorites');
     }
 
 
--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -12,8 +12,8 @@
                 <form method="post" action="/replies/{{$reply->id}}/favorites">
                     @csrf
                     <button type="submit" class="btn btn-secondary btn-sm" {{ $reply->isFavorited() ? 'disabled' : '' }}>
-                        {{ $reply->favorites()->count() }}
-                        {{ Str::plural('Favorite', $reply->favorites()->count())  }}
+                        {{ $reply->favorites_count }}
+                        {{ Str::plural('Favorite', $reply->favorites_count)  }}
                     </button>
                 </form>
 
