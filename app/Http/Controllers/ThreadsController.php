53305b3e488e15097a51630384f322fbf54cd682
Pc Elementary Os 2021 <bubu@outlook.com.pe>
2021-05-22 04:11:44 -0500

    Queries Down from 29 to 5
    yes.

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -44,7 +44,7 @@ class ThreadsController extends Controller
      */
     public function show($channelId, Thread $thread)
     {
-        
+
         return view('threads.show', [
             'thread'  => $thread,
             'replies' => $thread->replies()->paginate(5)
@@ -78,7 +78,7 @@ class ThreadsController extends Controller
      */
     protected function getThreads(Channel $channel, ThreadFilters $filters)
     {
-        $threads = Thread::latest()->filter($filters);
+        $threads = Thread::with('channel')->latest()->filter($filters);
         if ( $channel->exists ) {
             $threads->where('channel_id', $channel->id);
         }
