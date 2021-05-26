c789ebd0ed55a8774a91c9873bd3da74e34d4980
Pc Elementary Os 2021 <bubu@outlook.com.pe>
2021-05-26 07:32:26 -0500

    Just Refactoring N. 6

--- app/RecordsActivity.php
+++ app/RecordsActivity.php
@@ -25,6 +25,7 @@ trait RecordsActivity
 
     protected function getActivityType($event)
     {
-        return $event . '_' . strtolower((new \ReflectionClass($this))->getShortName());
+        $type = strtolower((new \ReflectionClass($this))->getShortName());
+        return "{$event}_{$type}";
     }
 }
