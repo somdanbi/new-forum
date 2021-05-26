e393422f371040b8f6f7f37bdd7a27f49934f1f2
Pc Elementary Os 2021 <bubu@outlook.com.pe>
2021-05-26 07:35:22 -0500

    Just Refactoring 7

--- app/RecordsActivity.php
+++ app/RecordsActivity.php
@@ -15,11 +15,9 @@ trait RecordsActivity
 
     public function recordActivity($event)
     {
-        Activity::create([
-            'user_id'      => auth()->id(),
-            'type'         => $this->getActivityType($event),
-            'subject_id'   => $this->id,
-            'subject_type' => get_class($this),
+        $this->activity()->create([
+            'user_id' => auth()->id(),
+            'type'    => $this->getActivityType($event),
         ]);
     }
 
@@ -27,5 +25,11 @@ trait RecordsActivity
     {
         $type = strtolower((new \ReflectionClass($this))->getShortName());
         return "{$event}_{$type}";
+    }
+
+    //Polimorphic relationship
+    public function activity()
+    {
+        return $this->morphMany('App\Activity','subject');
     }
 }
