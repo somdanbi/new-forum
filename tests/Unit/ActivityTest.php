f71d5136d4ec5c60234013cc46465e79ba8741a7
Pc Elementary Os 2021 <bubu@outlook.com.pe>
2021-05-26 11:22:23 -0500

    Testing ok 2

--- tests/Unit/ActivityTest.php
+++ tests/Unit/ActivityTest.php
@@ -45,14 +45,10 @@ class ActivityTest extends TestCase
         //given we have a thread
         $this->signIn();
 
-        create('App\Thread', [ 'user_id' => auth()->id() ]);
+        create('App\Thread', [ 'user_id' => auth()->id() ], 2);
+
         //and another thread from a week ago
-        create('App\Thread',[
-            'user_id' => auth()->id(),
-            'created_at' => Carbon::now()->subWeek(),
-
-        ]);
-
+        auth()->user()->activity()->first()->update([ 'created_at' => Carbon::now()->subWeek() ]);
         //when we fetch their feed
         $feed = Activity::feed(auth()->user());
 
@@ -60,6 +56,11 @@ class ActivityTest extends TestCase
         $this->assertTrue($feed->keys()->contains(
             Carbon::now()->format('Y-m-d')
         ));
+
+        $this->assertTrue($feed->keys()->contains(
+            Carbon::now()->subWeek()->format('Y-m-d')
+        ));
+
 
     }
 
