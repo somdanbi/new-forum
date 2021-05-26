92f22dbb0dd65396b151f7a00032036b40807096
Pc Elementary Os 2021 <bubu@outlook.com.pe>
2021-05-26 10:49:12 -0500

    Order by date

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -15,8 +15,6 @@ class ProfilesController extends Controller
                 return $activity->created_at->format('Y-m-d');
             });
 
-        return $activities;
-        
         return view('profiles.show', [
             'profileUser' => $user,
             'activities'  => $activities
--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -12,8 +12,12 @@
 
                 </div>
 
-                @foreach($activities as $activity)
-                    @include("profiles.activities.{$activity->type}")
+                @foreach($activities as $date => $activity)
+                    <h3>{{ $date }}</h3>
+                    @foreach($activity as $record)
+                        @include("profiles.activities.{$record->type}", ['activity' => $record])
+                    @endforeach
+
                 @endforeach
 
 {{--                {{$threads->links()}}--}}
