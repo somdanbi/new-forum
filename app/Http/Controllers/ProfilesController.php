79b4eac546b2f90ac04df2df0935124ee47b3588
Pc Elementary Os 2021 <bubu@outlook.com.pe>
2021-05-26 10:42:23 -0500

    Array: order by date

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -9,7 +9,14 @@ class ProfilesController extends Controller
 
     public function show(User $user)
     {
-        $activities = $user->activity()->latest()->with('subject')->get();
+        $activities = $user->activity()
+            ->latest()->with('subject')
+            ->get()->groupBy(function ($activity){
+                return $activity->created_at->format('Y-m-d');
+            });
+
+        return $activities;
+        
         return view('profiles.show', [
             'profileUser' => $user,
             'activities'  => $activities
