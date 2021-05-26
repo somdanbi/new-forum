475396efd2905d3a36c113577ef2f0f2cf510ddd
Pc Elementary Os 2021 <bubu@outlook.com.pe>
2021-05-26 09:45:57 -0500

    Array, profile: with subjects
    in page.test/profiles/Manu
    but it will show activities user aswell.

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -8,7 +8,7 @@ class ProfilesController extends Controller
 {
     public function show(User $user)
     {
-        return $user->activity;
+        return $user->activity()->with('subject')->get();
         return view('profiles.show', [
             'profileUser' => $user,
             'threads' => $user->threads()->paginate(15)
