0232c955427a06f3f31a0b3cd8850060ae11febf
Pc Elementary Os 2021 <bubu@outlook.com.pe>
2021-05-27 06:23:39 -0500

    30.1 recording activities

--- app/Favorite.php
+++ app/Favorite.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Favorite extends Model
 {
+
+    use RecordsActivity;
+
     protected $guarded = [];
 }
---
+++ app/me/logs/log_init.md
@@ -0,0 +1,2 @@
+#### commit 31.
+    - now recording in the db favorites in activities, (however is you check this activity in user profile, it breaks).
