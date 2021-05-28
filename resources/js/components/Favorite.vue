7b9835beb8c23783615464a95960abacff29b151
Pc Elementary Os 2021 <bubu@outlook.com.pe>
2021-05-28 11:58:30 -0500

    now toggle favorite reply is working

--- resources/js/components/Favorite.vue
+++ resources/js/components/Favorite.vue
@@ -12,7 +12,7 @@ export default{
     data(){
         return {
             favoritesCount: this.reply.favoritesCount,
-            isFavorited: false
+            isFavorited: this.reply.isFavorited
         }
     },
 
